bdd221cf8ece627aeb622ef7f7ba3e677ef02ff5
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2025-02-04 08:25:23 -0300

    chore: configure import routes aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+// IMPORTS
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { join } from 'path'
 
-// https://vite.dev/config/
+// CONFIG
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@lib': join(__dirname, './src/lib'),
+      '@hooks': join(__dirname, './src/hooks'),
+      '@contexts': join(__dirname, './src/contexts'),
+      '@screens': join(__dirname, './src/components/screens'),
+      '@ui': join(__dirname, './src/components/ui'),
+      '@workbench': join(__dirname, './src/components/workbench'),
+      '@components': join(__dirname, './src/components'),
+    },
+  },
 })
