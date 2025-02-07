3495d6614759c6766cd03d5ea7d476fa4b55aac1
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2025-02-07 07:30:47 -0300

    feat: add the cropper sides dragging feature
    - Add to the `CropperCalculator` module the functions to move the cropper sides.
    - Implements the `EditorClientConverter` module to convert client coords to real coords.
    - Now, the `EditorCropper` component allows the cropper sides to be dragged.

--- src/components/workbench/canvas/EditorCropper.tsx
+++ src/components/workbench/canvas/EditorCropper.tsx
@@ -1,8 +1,10 @@
 // IMPORTS
-import { useContext, useEffect, type ReactNode } from 'react'
+import { type MouseEvent, useContext, useEffect, type ReactNode } from 'react'
+import { EditorClientConverter } from '@lib/editor/ClientConverter'
 import { CropperCalculator } from '@lib/editor/CropperCalculator'
 import type { Dimensions, Position } from '@lib/common/types'
 import { useClasses } from '@hooks/common/useClasses'
+import { useDragging } from '@hooks/useDragging'
 import { EditorReferencesContext } from '@contexts/editor/EditorReferencesContext'
 import { EditorCropperContext } from '@contexts/editor/EditorCropperContext'
 import { ImageInputContext } from '@contexts/common/ImageInputContext'
@@ -23,6 +25,9 @@ export function EditorCropper (props: EditorCropperProps): ReactNode {
 
   // RENDER
   if (input.image === null) return null
+
+  // STATE
+  const dragging = useDragging()
 
   // EFFECT
   useEffect(() => {
@@ -57,11 +62,43 @@ export function EditorCropper (props: EditorCropperProps): ReactNode {
 
   }, [input.image])
 
+  // HANDLER
+  const cropperSideClickHandler = (event: MouseEvent<HTMLDivElement>, side: 'top' | 'right' | 'bottom' | 'left'): void => {
+    if (event.button !== 0) return
+    if (event.shiftKey || event.ctrlKey || event.metaKey || event.altKey) return
+    event.stopPropagation()
+    dragging.update((target) => {
+
+      if (input.image === null) return
+      if (cropper.values.current === null) return
+      if (references.preview.current === null) return
+
+      const calculator = CropperCalculator(input.image.dimensions, cropper.values.current)
+      const converter = EditorClientConverter(input.image.dimensions, references.preview.current)
+
+      const position = converter.relativeToImage(target)
+      let result = { ...cropper.values.current }
+
+      if (side === 'top') result = calculator.moveTopSide(position.y)
+      else if (side === 'right') result = calculator.moveRightSide(position.x)
+      else if (side === 'bottom') result = calculator.moveBottomSide(position.y)
+      else result = calculator.moveLeftSide(position.x)
+
+      cropper.setValues(result)
+
+    })
+  }
+
   // RENDER
   return <div className={useClasses(css.EditorCropper, props.className)}
     ref={references.cropper}
   >
-    {/*  */}
+
+    <div className={css.top} onMouseDown={(event): void => { cropperSideClickHandler(event, 'top') }}/>
+    <div className={css.right} onMouseDown={(event): void => { cropperSideClickHandler(event, 'right') }}/>
+    <div className={css.bottom} onMouseDown={(event): void => { cropperSideClickHandler(event, 'bottom') }}/>
+    <div className={css.left} onMouseDown={(event): void => { cropperSideClickHandler(event, 'left') }}/>
+
   </div>
 
 }
--- src/lib/editor/CropperCalculator.ts
+++ src/lib/editor/CropperCalculator.ts
@@ -20,5 +20,65 @@ export function CropperCalculator (image: Dimensions, cropper: Dimensions & Posi
 
     },
 
+    // FUNCTION
+    moveTopSide (y: number): Dimensions & Position {
+
+      const previous = { ...cropper }
+
+      const min = 0
+      const max = previous.y + previous.height - 20
+
+      y = Math.max(min, Math.min(y, max))
+
+      cropper.y = y
+      cropper.height = previous.y + previous.height - y
+
+      return cropper
+
+    },
+
+    // FUNCTION
+    moveRightSide (x: number): Dimensions & Position {
+
+      const previous = { ...cropper }
+
+      if (x < (previous.x + 20)) x = previous.x + 20
+      if (x > (image.width)) x = image.width
+
+      cropper.width = x - previous.x
+
+      return cropper
+
+    },
+
+    // FUNCTION
+    moveBottomSide (y: number): Dimensions & Position {
+
+      const previous = { ...cropper }
+
+      if (y < (previous.y + 20)) y = previous.y + 20
+      if (y > image.height) y = image.height
+
+      cropper.height = y - previous.y
+
+      return cropper
+
+    },
+
+    // FUNCTION
+    moveLeftSide (x: number): Dimensions & Position {
+
+      const previous = { ...cropper }
+
+      if (x < 0) x = 0
+      if (x > (previous.x + previous.width - 20)) x = previous.x + previous.width - 20
+
+      cropper.x = x
+      cropper.width = previous.width - (x - previous.x)
+
+      return cropper
+
+    },
+
   }
 }
